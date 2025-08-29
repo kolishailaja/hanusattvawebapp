cd63b71f05ecffd28dab1436d6c1b6a574929c92
kolishailaja <shailajak369@gmail.com>
2025-08-29 18:59:21 +0530

    first commit

---
+++ employee_dashboard-main/src/App.tsx
@@ -0,0 +1,52 @@
+import React, { useState, useEffect } from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { CartProvider } from './context/CartContext';
+import Navbar from './components/Navbar';
+import Loader from './components/Loader';
+import Home from './pages/Home';
+import Shop from './pages/Shop';
+import ProductDetail from './pages/ProductDetail';
+import Cart from './pages/Cart';
+import Login from './pages/Login';
+import Contact from './pages/Contact';
+import Quiz from './pages/Quiz';
+import Lookbook from './pages/Lookbook';
+
+const App: React.FC = () => {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    // Simulate loading time
+    const timer = setTimeout(() => {
+      setLoading(false);
+    }, 3000);
+
+    return () => clearTimeout(timer);
+  }, []);
+
+  if (loading) {
+    return <Loader />;
+  }
+
+  return (
+    <CartProvider>
+      <Router>
+        <div className="App">
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/shop" element={<Shop />} />
+            <Route path="/product/:id" element={<ProductDetail />} />
+            <Route path="/cart" element={<Cart />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/contact" element={<Contact />} />
+            <Route path="/quiz" element={<Quiz />} />
+            <Route path="/lookbook" element={<Lookbook />} />
+          </Routes>
+        </div>
+      </Router>
+    </CartProvider>
+  );
+};
+
+export default App;
